7fa311bede0b2b93c6577e0da987559a3c2cc6e9
Micro Fortwonatus <microfortwonatus6@gmail.com>
2024-08-30 04:39:35 +0700

    API Routes

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 import Content from "@/components/content";
 import Layout from "@/layout";
+import { useEffect } from "react";
 
 export default function Main() {
+
+useEffect(() => {
+    fetch("api/hello")
+    .then((res) => res.json())
+    .then((res) => console.log("response =>", res))
+    .catch((err) => console.log("error =>", err))
+}, [])
  return (
   <Layout metaTitle="home">
-  <p>ini adalah halaman Home</p>
+  <p>Home</p>
   </Layout>
  );
 }
