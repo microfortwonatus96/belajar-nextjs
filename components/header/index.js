8663f4c629a7d7ff49c3d933d467e298535dad01
Micro Fortwonatus <microfortwonatus6@gmail.com>
2024-08-30 03:58:29 +0700

    styling

--- components/header/index.js
+++ components/header/index.js
@@ -1,9 +1,10 @@
 import Link from "next/link"
-// import styles from './styles.module.css'
+import styles from './styles.module.css'
 
 export default function Header() {
-    return <div >
-        <ul className="d-flex">
+    return <div className={styles.header}>
+        <div>Logo</div>
+        <ul className="flex gap-3 text-nav">
             <li><Link href="/">Home</Link></li>
             <li><Link href="/profile">Profile</Link></li>           
             <li><Link href="/users">Users</Link></li>           
